794656666cafbbe8445fb2d7568a6b13aa52c600
Tran Manh Chung <chungtran2801@gmail.com>
2019-04-04 20:14:17 +0700

    update model

--- src/models/Member.js
+++ src/models/Member.js
@@ -3,6 +3,7 @@ var mongoose = require('mongoose')
 const Schema = mongoose.Schema
  
 const MemberSchema = new Schema({
+  userID: String,
   name: String,
   idCard: String,
   phone: String,
--- src/models/Room.js
+++ src/models/Room.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose')
 const Schema = mongoose.Schema
  
 const RoomSchema = new Schema({
-    houseId: Schema.Types.ObjectId,
+    userID: String,
     name: String,
     address: String,
     status:{
