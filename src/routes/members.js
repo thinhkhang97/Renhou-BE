1f59f4b6a0101c335ee1ac469f9589731365ab2d
nguditi <tienandehit@gmail.com>
2019-04-21 12:54:50 +0700

    update authenticate API room,member

--- src/routes/members.js
+++ src/routes/members.js
@@ -4,40 +4,60 @@ var MemberModel = require('../models/Member');
 var RoomModel = require('../models/Room');
 
 const handlePageError = (res, e) => res.status(500).send(e.message)
+const handleAccessDeny = (res) =>  res.status(403).send({messase: 'Access deny',})
+
 
 //add new members to the room
 router.post(
   '/create/:idRoom',
   async (req, res) => {
-    try {
-      const room = (await RoomModel.findById(req.params.idRoom));
-      req.body.userId = room.userId;
-      const memberDoc = new MemberModel(req.body);
-      const member = await memberDoc.save();
-      //add user to the room
-      const listUserInRoom = room.membersIdNumber;
-      listUserInRoom.push(member._id);
-      await RoomModel.findOneAndUpdate( req.params.idRoom, {membersIdNumber: listUserInRoom,status:'used'});
-      return res.send({
-        message: 'Created new member successfully!',
-        data: member
-      })
-    } catch (e) {
-      return handlePageError(res, e)
+    if(req.user._id != req.body.userId)
+     return handleAccessDeny(res)
+    else{
+        try {
+          const room = (await RoomModel.findById(req.params.idRoom));
+          if(req.body.userId != room.userId)
+          {
+            return handleAccessDeny(res)
+          }
+          else{
+            const memberDoc = new MemberModel(req.body);
+            const member = await memberDoc.save();
+            //add user to the room
+            const listUserInRoom = room.membersIdNumber;
+            listUserInRoom.push(member._id);
+            await RoomModel.findOneAndUpdate( req.params.idRoom, {membersIdNumber: listUserInRoom,status:'used'});
+            return res.send({
+              message: 'Created new member successfully!',
+              data: member
+            })
+          }
+        } catch (e) {
+          return handlePageError(res, e)
+        }
+      }
     }
-  }
 )
 
 //update infor of a member
 router.put(
   '/update/:id',
   async (req, res) => {
-    try {
-      await MemberModel.findOneAndUpdate(req.params.id, req.body)
-
-      return res.json({ message: 'Updated Member successfully!' })
-    } catch (e) {
-      return handlePageError(res, e)
+    if(req.user._id != req.body.userId)
+      return handleAccessDeny(res)
+    else{
+      try {
+        const member = (await MemberModel.findById(req.params.id));
+        if(req.body.userId != member.userId){
+          return handleAccessDeny(res)
+        }
+        else{
+          await MemberModel.findOneAndUpdate(req.params.id, req.body)
+          return res.json({ message: 'Updated Member successfully!' })
+        }
+      } catch (e) {
+        return handlePageError(res, e)
+      }
     }
   }
 )
@@ -46,15 +66,19 @@ router.put(
 router.get(
   '/get/all/:userid',
   async (req, res) => {
-    try {
-      await MemberModel.find({userId: req.params.userid, delFlag : false}, function(err, member){
-        if(err){
-          return res.status(404).send({ message: 'Not found!' })
-        }
-          return res.json({ member: member })
-      })
-    } catch (e) {
-      return handlePageError(res, e)
+    if(req.user._id != req.params.userid)
+      return handleAccessDeny(res)
+    else{
+      try {
+        await MemberModel.find({userId: req.params.userid, delFlag : false}, function(err, member){
+          if(err){
+            return res.status(404).send({ message: 'Not found!' })
+          }
+            return res.json({ member: member })
+        })
+      } catch (e) {
+        return handlePageError(res, e)
+      }
     }
   }
 )
@@ -63,16 +87,26 @@ router.get(
 router.get(
   '/get/:id',
   async (req, res) => {
-    try {
-      await MemberModel.find({_id : req.params.id, delFlag : false}, function(err, member){
-        if(err){
-          return res.status(404).send({ message: 'Not found member with id ' + req.params.id + ' !' })
-        }
-          return res.json({ member: member })
-      })
-    } catch (e) {
-      return handlePageError(res, e)
-    }
+    if(req.user._id != req.query.userId)
+      return handleAccessDeny(res)
+    else{
+      try {
+        await MemberModel.findOne({_id : req.params.id, delFlag : false}, function(err, member){
+          if(err){
+            return res.status(404).send({ message: 'Not found member with id ' + req.params.id + ' !' })
+          }
+          else if(member.userId != req.query.userId)
+          {
+            return handleAccessDeny(res);
+          }
+          else{
+            return res.json(member)
+          }
+        })
+      } catch (e) {
+        return handlePageError(res, e)
+      }
+    } 
   }
 )
 
@@ -80,12 +114,21 @@ router.get(
 router.delete(
   '/delete/:id',
   async (req, res) => {
-    try {
-      await MemberModel.findOneAndUpdate(req.params.id, {delFlag:true})
-
-      return res.json({ message: 'Deleted Member successfully!' })
-    } catch (e) {
-      return handlePageError(res, e)
+    if(req.user._id != req.query.userId)
+      return handleAccessDeny(res)
+    else{
+      try {
+        const member = await MemberModel.findById(req.params.id);
+        if(req.query.userId != member.userId){
+          return handleAccessDeny(res)
+        }
+        else{
+          await MemberModel.findOneAndUpdate(req.params.id, {delFlag:true})
+          return res.json({ message: 'Deleted Member successfully!' })
+        } 
+      } catch (e) {
+        return handlePageError(res, e)
+      }
     }
   }
 )
@@ -94,15 +137,19 @@ router.delete(
 router.post(
   '/room/get',
   async (req, res) => {
-    try {
-      await MemberModel.find({'_id' : { $in: req.body.listMembersId}, delFlag : false}, function(err, members){
-        if(err){
-          return res.status(404).send({ message: 'Not found!' })
-        }
-          return res.json({ member: members })
-      })
-    } catch (e) {
-      return handlePageError(res, e)
+    if(req.user._id != req.body.userId)
+      return handleAccessDeny(res)
+    else{
+      try {
+        await MemberModel.find({'_id' : { $in: req.body.listMembersId}, delFlag : false, userId: req.body.userId}, function(err, members){
+          if(err){
+            return res.status(404).send({ message: 'Not found!' })
+          }
+            return res.json({ member: members })
+        })
+      } catch (e) {
+        return handlePageError(res, e)
+      }
     }
   }
 )
--- src/routes/room.js
+++ src/routes/room.js
@@ -4,114 +4,133 @@ var Room = require('../models/Room');
 var Rule = require('../models/FeeRule');
 var passport = require('passport');
 
+const handlePageError = (res, e) => res.status(500).send(e.message)
+const handleAccessDeny = (res) =>  res.status(403).send({messase: 'Access deny',})
+
 //Add a new room with fee rule
 router.post('/',(req, res) => {
-  const room = new Room(req.body);
-  room.save((e,newRoom)=>{
-    if(e)
-      res.status(500).send({
-          messase: 'error',
-          data: e
-      })
-    else
-    {
-      req.body.roomId = newRoom._id;
-      const rule = new Rule((req.body));
-      rule.save((e1,newRule)=>{
-        if(e1)
-          res.status(500).send({
-              messase: 'error',
-              data: e1
-          })
-        else
-          res.send({
-            message: 'Created new room successfully!',
-            data: {
-              room: newRoom,
-              rule: newRule
-            }
-          })
-      })
-    }
-  })
+  if(req.user._id != req.body.userId)
+    return handleAccessDeny(res);
+  else{
+    const room = new Room(req.body);
+    room.save((e,newRoom)=>{
+      if(e)
+        return handlePageError(res,e);
+      else
+      {
+        req.body.roomId = newRoom._id;
+        const rule = new Rule((req.body));
+        rule.save((e1,newRule)=>{
+          if(e1)
+            return handlePageError(res,e1);
+          else
+            res.send({
+              message: 'Created new room successfully!',
+              data: {
+                room: newRoom,
+                rule: newRule
+              }
+            })
+        })
+      }
+    })
+  }
 })
 
 
 //Configurate list member
 router.put('/members',(req, res) => {
-  Room.findOneAndUpdate({_id:req.body.roomId},{membersIdNumber:req.body.listMember}).exec((e,data)=>{
-    if(e)
-      res.status(500).send({
-          messase: 'error',
-          data: e
-      })
-    else
-      res.send({
-          messase: 'configurate success',
-      })
-  }) 
+  if(req.user._id != req.body.userId)
+    return handleAccessDeny(res);
+  else{
+    Room.findOne({_id:req.body.roomId}).exec((e,data)=>{
+      if(e || !data)
+        return handlePageError(res,e);
+      if(data.userId != req.body.userId)
+      {
+        return handleAccessDeny(res);
+      }
+      else{
+        Room.updateOne({_id:req.body.roomId},{membersIdNumber:req.body.listMember}).exec((e)=>{
+          if(e)
+            return handlePageError(res,e);
+          res.send({
+            messase: 'configurate success',
+          })
+        }) 
+      }
+    }) 
+  }
 })
 
-//Configurate room
+//Configurate room and rule fee
 router.put('/config',(req, res) => {
-  console.log(req.body)
-  Room.findOneAndUpdate({_id:req.body.roomId},req.body).exec((e,data)=>{
-    if(e)
-      res.status(500).send({
-          messase: 'error',
-          data: e
-      })
-  }) 
-  Rule.findOneAndUpdate({roomId:req.body.roomId},req.body).exec((e,data)=>{
-    if(e)
-      res.status(500).send({
-          messase: 'error',
-          data: e
-      })
-  }) 
-  res.send({
-  messase: 'configurate success'});
+  if(req.user._id != req.body.userId)
+    return handleAccessDeny(res);
+  else{
+    Room.findOne({_id:req.body.roomId}).exec((e,data)=>{
+      if(e || !data)
+        return handlePageError(res,e);
+      if(data.userId != req.body.userId)
+      {
+        return handleAccessDeny(res)
+      }
+      else{
+        Room.updateOne({_id:req.body.roomId},req.body).exec((e1)=>
+        {
+          if(e1)
+            return handlePageError(res,e1);
+        })
+        Rule.findOneAndUpdate({roomId:req.body.roomId},req.body).exec((e2)=>{
+          if(e)
+            return handlePageError(res,e2);
+          else
+            return res.status(200).send({
+              messase: 'configurate success'});
+        }) 
+      }
+    }) 
+  }
 })
 
 //Find all room of an owner base on owner _id
 router.get('/all/:userid', (req, res) => {
-  Room.find({userId: req.params.userid}).exec((e,data)=>{
-    if(e)
-      res.status(500).send({
-          messase: 'error',
-          data: e
-      })
-    else
-      res.send({
-          data: data
-      })
-  }) 
+  if(req.user._id != req.params.userid)
+    return handleAccessDeny(res)
+  else{
+    Room.find({userId: req.params.userid}).exec((e,data)=>{
+      if(e)
+        return handlePageError(res,e);
+      else
+        res.send({
+            data: data
+        })
+    }) 
+  }
 })
 
 //Find a room base on _id 
-router.get('/:id', (req, res) => {
-  Room.findOne({_id: req.params.id}).exec((e,data)=>{
-    if(e)
-      res.status(500).send({
-          messase: 'error',
-          data: e
-      })
-    else
-      Rule.findOne({roomId: req.params.id}).exec((e2,extradata)=>{
-        if(e2)
-          res.status(500).send({
-              messase: 'error',
-              data: e2
-          })
-        else
-          res.send({
-              data: {
-                room: data,
-                rule: extradata
-              }
-          })
-      }) 
-  }) 
+router.get('/:roomId', (req, res) => {
+  if(req.user._id != req.query.userId)
+    return handleAccessDeny(res)
+  else{
+    Room.findOne({_id: req.params.roomId}).exec((e,data)=>{
+      if(e)
+        return handlePageError(res,e);
+      else
+        Rule.findOne({roomId: req.params.roomId}).exec((e1,extradata)=>{
+          if(e1)
+            return handlePageError(res,e1);
+          else
+            res.send({
+                data: {
+                  room: data,
+                  rule: extradata
+                }
+            })
+        }) 
+    }) 
+  }
 })
 
 //Find a room base on address and name 
